d778303157be778d5191f90c3c526e3e2f9e4d0a
Edward Han <edwardhuahan@gmail.com>
2022-10-02 16:05:25 -0400

    Added backend docker configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    sayMyName();
+  }, []);
+
+  const sayMyName = () => {
+    fetch('/hello?myName=React App')
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
