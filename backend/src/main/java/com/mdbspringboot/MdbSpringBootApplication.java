4f84c8cf687a38b1fc1ad304397d391eb5dc481a
Edward Han <edwardhuahan@gmail.com>
2022-10-05 16:46:49 -0400

    Added imports for mongo setup

--- backend/src/main/java/com/mdbspringboot/MdbSpringBootApplication.java
+++ backend/src/main/java/com/mdbspringboot/MdbSpringBootApplication.java
@@ -6,13 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude={MongoAutoConfiguration.class})
+@SpringBootApplication
 @EnableMongoRepositories
 @RestController
 public class MdbSpringBootApplication implements CommandLineRunner {
