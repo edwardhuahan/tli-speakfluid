3f2e7309974b1434316a4c7f451753f6bb54fef9
zhgkj <zhgkaijun@gmail.com>
2022-10-04 20:45:37 -0400

    Documented ItemRepository

--- backend/src/main/java/com/mdbspringboot/repository/ItemRepository.java
+++ backend/src/main/java/com/mdbspringboot/repository/ItemRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.mongodb.repository.Query;
 import com.mdbspringboot.model.Transcript;
 
 public interface ItemRepository extends MongoRepository<Transcript, String> {
+    // This is the API implementation. This acts as a link between the MongoDB model and the database.
 
+    // findItemByName is a query, we specify the parameter it takes with @Query. This searches through the collection
+    // and returns the first item it finds with the matching name.
     @Query("{name:'?0'}")
     Transcript findItemByName(String name);
 
+    // We use count to count how long the collection is.
     public long count();
 
 }
