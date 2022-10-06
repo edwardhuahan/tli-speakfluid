89c78d413b104772baa3a09db95722127a6f280a
Edward Han <edwardhuahan@gmail.com>
2022-10-05 21:49:39 -0400

    Quick change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,6 @@ function App() {
 
   const [name, setName] = useState("")
 
-  useEffect(() => {
-    sayMyName();
-  }, []);
-
   const sayMyName = () => {
     fetch('/hello?myName=' + name)
       .then(response => response.text())
