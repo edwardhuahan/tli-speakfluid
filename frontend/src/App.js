b6c3b80b61b8124fecf50d898fbc4f44d97f1ae5
Sarah <lemiex@outlook.com>
2022-10-06 00:27:59 -0400

    changed to App.js, d isplayed transcript data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,8 @@ function App() {
 
   const [message, setMessage] = useState("")
   const [transcripts, setTranscripts] = useState()
-
   const [name, setName] = useState("")
+  let parsedTranscripts = []
 
   useEffect(() => {
     sayMyName();
@@ -25,11 +25,11 @@ function App() {
   const voiceFlowAPI = () => {
     // Simple GET request using fetch
     fetch('https://warm-everglades-89279.herokuapp.com/https://api-dm-test.voiceflow.fr/exportraw/VF.DM.6331204c9575ca00085c3fee.3xaArTcugE4obpnp?versionID=632b79c564484143a984b02e')
-        .then(response => response.json())
-        .then(data => console.log(data));
+      .then(response => response.json())
+      .then(data => { setTranscripts([...data]) });
   }
 
-  const handleChange = (event: any) => {
+  const handleChange = (event) => {
     setName(event.target.value);
   }
 
@@ -37,13 +37,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input 
-        onChange={handleChange}
+        <input
+          onChange={handleChange}
         ></input>
-
         <button onClick={sayMyName}> Enter </button>
         <p>
           {message}
+        </p>
+        <p>
+          {transcripts && transcripts[0].map(t => {
+            return (
+              <li>
+                <h5>turnID: {t.turnID}</h5>
+                <h5>startTime: {t.startTime}</h5>
+              </li>
+            );
+          })
+          }
         </p>
       </header>
     </div>
