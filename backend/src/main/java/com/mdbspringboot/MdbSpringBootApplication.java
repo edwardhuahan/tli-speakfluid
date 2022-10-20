32aa223c53aea7c275f3ac7b1e49d75eaab4dcb9
zhgkj <zhgkaijun@gmail.com>
2022-10-20 01:42:28 -0400

    POST req bug.

--- backend/src/main/java/com/mdbspringboot/MdbSpringBootApplication.java
+++ backend/src/main/java/com/mdbspringboot/MdbSpringBootApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @EnableMongoRepositories
 @RestController
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
+import Home from './pages/Home';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
