073d2bed0050f7ac9383797d124fe7a86fb8fab7
Sarah <lemiex@outlook.com>
2022-11-01 13:56:42 -0400

    merged 30-sf028-reproduce-mockup-with-react-typescript with Kai's new changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,24 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input
-          onChange={handleChange}
-        ></input>
-        <button onClick={sayMyName}> Enter </button>
-        <p>
-          {message}
-        </p>
-        <p>
-          {transcripts && transcripts[0].map((t: Transcript) => {
-            return (
-              <li>
-                <h5>turnID: {t.turnID}</h5>
-                <h5>startTime: {t.startTime}</h5>
-              </li>
-            );
-          })
-          }
-        </p>
+
       </header>
     </div>
   );
