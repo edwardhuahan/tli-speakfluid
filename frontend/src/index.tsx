dae1465ed4f59764ab6c756797527ccee571e1fd
Edward Han <edwardhuahan@gmail.com>
2022-11-03 03:10:58 -0400

    Quick fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import Home from './Pages/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
