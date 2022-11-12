c8be0186789edfe9decc90a4267bcd591f55b05d
auroraborie <auroraaurora.zhang@mail.utoronto.ca>
2022-11-12 16:07:16 -0500

    created entities package containing FindOptimalTalkStep and CalculateConfidenceScore

--- backend/src/main/java/com/speakfluid/backend/entities/Suggestion.java
+++ backend/src/main/java/com/speakfluid/backend/entities/Suggestion.java
@@ -1,17 +1,7 @@
 java.import.util.*;
 
 public class Suggestion {
-    HashMap<String, Integer> talkStepToScoreMapping = new HashMap<String, Integer>();
-    StepManager stepManager;
-    public Suggestion(StepManager manager) {
-        this.stepManager = manager;
-    }
 
-    public double callManager(){
-
-
-
-    }
 
 
 
