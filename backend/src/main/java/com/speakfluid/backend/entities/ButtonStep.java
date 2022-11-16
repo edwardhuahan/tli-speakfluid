c3c4a3cb99ca65737e2959ed028a92ce3a8daea6
auroraborie <auroraaurora.zhang@mail.utoronto.ca>
2022-11-16 17:01:21 -0500

    Implemented ButtonStep, CardStep, SuggestionManager, and StepManager in accordance with our CRC cards design.

---
+++ backend/src/main/java/com/speakfluid/backend/entities/ButtonStep.java
@@ -0,0 +1,78 @@
+package com.speakfluid.backend.entities;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+/**
+ * The ButtonStep class runs the inputted Dialogue object through all metrics for calculating the probability of
+ * the usefulness of a button for this step in the conversational flow. It implements the TalkStep abstract class
+ * and inherits all of its attributes and methods.
+ * @author Aurora Zhang
+ * @version 1.0
+ * @since November 16th 2022
+ */
+
+public class ButtonStep extends TalkStep{
+
+    private double scoreAccumulator;
+    private final double maxScore = 12;
+    private final String stepName;
+    int chatbotMsgLength;
+    int userMsgLength;
+
+    public ButtonStep(){
+        this.stepName = "button step";
+        this.scoreAccumulator = 0;
+    }
+    private final ArrayList<Map<String, Double>> chatbotKeywordsScoreMap = (ArrayList<Map<String, Double>>) Arrays.asList(
+            Map.ofEntries(entry("would you", 2.0),
+                    entry("what type", 2.0), entry("are you", 2.0), entry("would it", 2.0),
+                    entry("what kind", 2.0), entry("here are", 2.0), entry("do you", 2.0),
+                    entry("here is", 2.0)),
+            Map.ofEntries(entry("here are", 4.0), entry("choose", 4.0), entry("select", 4.0)),
+            Map.ofEntries(entry("destination", 3.0), entry("date", 3.0), entry("departure", 3.0),
+                    entry("arriving", 3.0)));
+    private final ArrayList<Map<String, Double>> userKeywordsScoreMap = (ArrayList<Map<String, Double>>) Arrays.asList(
+            Map.ofEntries(entry("booking", 2.0),
+                    entry("train", 2.0), entry("go to", 2.0), entry("arrive", 2.0)),
+            Map.ofEntries(entry("hotel", 4.0), entry("cheap", 4.0), entry("hospital", 4.0)));
+
+    @Override
+    public String getStepName() {
+        return stepName;
+    }
+
+    /**
+     * runs each method to analyze both the user and chatbot output to see whether this dialogue is
+     * best suited for the use of a button. Updates the accumulated score attribute accordingly.
+     * @param dialogue a single back and forth between the user and the chatbot.
+     */
+
+    @Override
+    public void runAnalysis(Dialogue dialogue) {
+        for(Speech speech: dialogue.getChatBotMessage()){
+            countMatchKeywords(speech, chatbotKeywordsScoreMap);
+            chatbotMsgLength = calculateMsgLength(speech);
+
+        }
+        for(Speech speech: dialogue.getUserMessage()){
+            countMatchKeywords(speech, userKeywordsScoreMap);
+            userMsgLength = calculateMsgLength(speech);
+
+        }
+        // if the chatbot outputs more information than the user, buttons are not suitable
+        if((chatbotMsgLength/userMsgLength) > 1){
+            this.scoreAccumulator -= 2;
+        }
+        else{
+            this.scoreAccumulator += 2;
+        }
+        // if the user response is short, then this suggests buttons are suitable
+        if(userMsgLength <= 5){
+            this.scoreAccumulator += 2;
+        }
+    }
+}
---
+++ backend/src/main/java/com/speakfluid/backend/entities/StepManager.java
@@ -0,0 +1,39 @@
+package com.speakfluid.backend.entities;
+
+/**
+ * StepManager is responsible for calling every method for each child of TalkStep to increment or decrement their
+ * corresponding accumulated scores as necessary. It then returns the confidence score of the TalkStep child based on the
+ * inputted Dialogue object.
+ * @author Aurora Zhang
+ * @version 1.0
+ * @since November 16th, 2022
+ */
+
+public class StepManager implements CalculateConfidenceScore{
+    double stepScoreAccumulator;
+    double stepTotalScore;
+
+    /**
+     * calls each method of the TalkStep to calculate how many of our metrics for the optimal
+     * talk step it satisfies. Keeps track of the accumulated score and max score in the stepTotalScore
+     * and stepStoreAccumulator attributes.
+     * @param dialogue the single back and forth between the user and chatbot.
+     * @param step the type of the talk step whose methods are called.
+     */
+
+    public void passDialogueToTalkStep(Dialogue dialogue, TalkStep step) {
+        step.runAnalysis(dialogue);
+        stepTotalScore = step.getMaxScore();
+        stepScoreAccumulator = step.getScoreAccumulator();
+    }
+
+    /**
+     * Calculates the confidence score of the talk step.
+     * @return returns the confidence score of the talk step.
+     */
+    @Override
+    public double calculateConfidenceScore() {
+        return (stepScoreAccumulator / stepTotalScore) * 100;
+    }
+
+}
