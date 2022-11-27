e1c40d56a5d8f28b0b30ca56a9844817f2f047a0
ZZBP <zeling.zhang@mail.utoronto.ca>
2022-11-27 11:20:04 -0500

    Updated comments in ScoreStandards

--- backend/src/main/java/com/speakfluid/backend/entities/ScoreStandards.java
+++ backend/src/main/java/com/speakfluid/backend/entities/ScoreStandards.java
@@ -10,7 +10,11 @@ public class ScoreStandards {
     public static double highMatch = 5.0;
     public static double mediumMatch = 3.0;
     public static double lowMatch = 1.0;
+    // a standard step class is considered as a class with only one set of keywords.
     public static double standardStepClass = 15.0;
+    // Additional Analysis includes a second set of keywords and or additional analysis method.
+    // For example, TextStep has two sets of keywords and one additional method.
+    // Its maxScore is standdardStepClass + 2 * additionalAnalysis.
     public static double additionalAnalysis = 3.0;
 
 }
